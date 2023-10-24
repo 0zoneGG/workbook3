4f21c90d2121f44ded8f9d77624e56fec5526d00
0zoneGG <jcrojas408@gmail.com>
2023-10-24 14:15:16 -0700

    favoriteThings List Commit

---
+++ simple_functions.js
@@ -0,0 +1,22 @@
+// Function to display favorite things
+function favoriteThings() {
+    console.log("My name is Juan.");
+    console.log("My favorite movie is click.");
+    console.log("My favorite musician/band is Edicion Especial.");
+}
+
+// Function to explain Im here
+function whyImHere() {
+    console.log("I am here because I want a meaningful career");
+}
+
+//Function to name favorite palces
+
+function favoritePlace() {
+    console.log("My favorite place is Nayarit, Mexico because thats where I grow up");
+}
+
+// Call the functions
+favoriteThings();
+whyImHere();
+favoritePlace();
